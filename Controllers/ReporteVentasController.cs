ee963df87ac670972161b38db7a9fede6b4c1179
3lPr1mo <erickenriquechaparro@gmail.com>
2023-11-29 16:50:46 -0500

    Some changes

---
+++ Controllers/ReporteVentasController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoWeb.Controllers
+{
+    public class ReporteVentasController : Controller
+    {
+        // GET: ReporteVentas
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

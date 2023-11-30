4389b2599aa8dac5b81d5a3818f73a35758ae49a
Johan Duarte <jyduartes@udistrital.edu.co>
2023-11-29 21:59:05 -0500

    report sales

--- Controllers/ReporteVentasController.cs
+++ Controllers/ReporteVentasController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProyectoWeb.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,11 @@ namespace ProyectoWeb.Controllers
 {
     public class ReporteVentasController : Controller
     {
+        nowhereluvBDEntities bddatos = new nowhereluvBDEntities();
         // GET: ReporteVentas
         public ActionResult Index()
         {
-            return View();
+            return View(bddatos.Sales.ToList());
         }
     }
 }

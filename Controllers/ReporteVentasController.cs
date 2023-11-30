d1d19c527078317e3c08e25b5c3d880971e9ea70
Johan Duarte <jyduartes@udistrital.edu.co>
2023-11-29 23:24:45 -0500

    Reporte ventas conseguido mas o menos

--- Controllers/ReporteVentasController.cs
+++ Controllers/ReporteVentasController.cs
@@ -1,9 +1,12 @@
-﻿using ProyectoWeb.Models;
+﻿using Microsoft.Reporting.WebForms;
+using ProyectoWeb.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace ProyectoWeb.Controllers
 {
@@ -13,7 +16,34 @@ namespace ProyectoWeb.Controllers
         // GET: ReporteVentas
         public ActionResult Index()
         {
-            return View(bddatos.Sales.ToList());
+            var sales = bddatos.Sales.Include(s => s.Inventory).Include(s => s.User);
+            return View(sales.ToList());
+        }
+        public ActionResult VistaReporteVentas(string id ) { 
+        
+            LocalReport reporte = new LocalReport();
+            string ruta = Path.Combine(Server.MapPath("~/Reportes"), "ReportVentas.rdlc");
+            reporte.ReportPath = ruta;
+            List<Sales> listado = new List<Sales>();
+            listado= bddatos.Sales.ToList();
+            ReportDataSource verreporte = new ReportDataSource("Datos_Venta", listado);
+            reporte.DataSources.Add(verreporte);
+            String tiporeporte = id;
+            string mime, codificacion, archivo;
+            string[] flujo;
+            Warning[] aviso;
+            string dispositivo = @"<DeviceInfo>" +
+             " <OutputFormat>" + id + "</OutputFormat>" +
+             " <PageWidth>8.5in</PageWidth>" +
+             " <PageHeight>11in</PageHeight>" +
+             " <MarginTop>0.5in</MarginTop>" +
+             " <MarginLeft>1in</MarginLeft>" +
+             " <MarginRight>1in</MarginRight>" +
+             " <MarginBottom>0.5in</MarginBottom>" +
+             " <EmbedFonts>None</EmbedFonts>" +
+             "</DeviceInfo>";
+            byte[] enviar = reporte.Render(id, dispositivo, out mime, out codificacion, out archivo, out flujo, out aviso);                   
+            return File(enviar, mime);
         }
     }
 }
